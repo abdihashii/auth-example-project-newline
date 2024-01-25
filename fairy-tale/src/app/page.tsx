2599b30f5af5bc61a9530834f491ebb6c3e6dca5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-25 14:32:03 -0600

    Make the homepage not show authorized content

--- fairy-tale/src/app/page.tsx
+++ fairy-tale/src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-	return <main className="">Main Page</main>;
+import { Button } from '@/components/ui/button';
+import { getUser } from '@/lib/supabaseServerClient';
+import Link from 'next/link';
+
+export default async function Home() {
+	const user = await getUser();
+
+	return (
+		<main className="flex h-screen flex-col items-center justify-center gap-8">
+			<h1 className="inline-block bg-gradient-to-r from-blue-600  via-red-500 to-indigo-400 bg-clip-text text-4xl font-bold text-transparent">
+				Welcome to Fairy Tale
+			</h1>
+
+			{user ? (
+				<section className="max-w-md">
+					<p>You are signed in</p>
+					{/* <Button>Sign out</Button> */}
+				</section>
+			) : (
+				<section className="flex max-w-md flex-col gap-4">
+					<p>Please sign in to continue</p>
+
+					<Link href="/auth/signin">
+						<Button className="w-full">Sign in</Button>
+					</Link>
+				</section>
+			)}
+		</main>
+	);
 }
