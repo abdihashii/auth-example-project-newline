9f51732365102f45bd5fbc06884ffdd32b87d15f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-26 23:15:06 -0600

    Redirect to home page if user is already signed in from sign in page

--- fairy-tale/src/app/auth/signin/page.tsx
+++ fairy-tale/src/app/auth/signin/page.tsx
@@ -1,6 +1,14 @@
 import SignInForm from '@/components/Auth/SignInForm';
+import { getUser } from '@/lib/supabaseServerClient';
+import { redirect } from 'next/navigation';
 
-export default function SignIn() {
+export default async function SignIn() {
+	const user = await getUser();
+
+	if (user) {
+		redirect('/');
+	}
+
 	return (
 		<main className="flex h-screen items-center justify-center bg-[#F8F9FA]">
 			<SignInForm />
