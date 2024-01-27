2f194db3d4c68fe4d3e7cbfc3332217cf5a61f51
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-26 23:14:29 -0600

    Add a protected page route with a redirect to sign in if not logged in

---
+++ fairy-tale/src/app/protected/page.tsx
@@ -0,0 +1,20 @@
+import { getUser } from '@/lib/supabaseServerClient';
+import { redirect } from 'next/navigation';
+
+export default async function ProtectedPage() {
+	const user = await getUser();
+
+	if (!user) {
+		redirect('/auth/signin');
+	}
+
+	return (
+		<main className="flex h-screen items-center justify-center bg-[#F8F9FA]">
+			<section className="flex w-full max-w-md flex-col items-center space-y-8 rounded-lg bg-white p-6 shadow-lg">
+				<h1>This page is protected</h1>
+
+				<p>Hello, {user.email}</p>
+			</section>
+		</main>
+	);
+}
