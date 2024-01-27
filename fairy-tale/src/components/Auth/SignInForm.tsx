ffd991741c7e7e76798ab05f4b81d248be31c22c
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-26 22:43:14 -0600

    Add form validation

--- fairy-tale/src/components/Auth/SignInForm.tsx
+++ fairy-tale/src/components/Auth/SignInForm.tsx
@@ -1,19 +1,49 @@
 'use client';
 
-import Link from 'next/link';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
-import { EyeIcon, EyeOff, Feather } from 'lucide-react';
-import { Button } from '../ui/button';
-import { FcGoogle } from 'react-icons/fc';
-import { FaGithub } from 'react-icons/fa';
 import { useState } from 'react';
+import Link from 'next/link';
+import * as z from 'zod';
+import { FieldValues, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { EyeIcon, EyeOff, Feather } from 'lucide-react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import useAuth from '@/hooks/useAuth';
+
+const signInSchema = z.object({
+	email: z.string().email({
+		message: 'Please enter a valid email',
+	}),
+	password: z.string().min(1, {
+		message: 'Please enter a password',
+	}),
+});
 
 const SignInForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(signInSchema),
+	});
+
 	const [showPassword, setShowPassword] = useState(false);
+	const { handleSignInWithEmail } = useAuth();
+
+	const onSubmit = (data: FieldValues) => {
+		handleSignInWithEmail(data);
+	};
 
 	return (
-		<form className="w-full max-w-md space-y-6 rounded-lg bg-white p-6 shadow-lg">
+		<form
+			className="w-full max-w-md space-y-8 rounded-lg bg-white p-6 shadow-lg"
+			onSubmit={handleSubmit(onSubmit)}
+		>
 			<section className="flex items-center justify-center gap-2">
 				<Feather className="h-8 w-8" />
 				<h1 className="inline-block bg-gradient-to-r from-blue-600  via-red-500 to-indigo-400 bg-clip-text text-2xl font-bold text-transparent">
@@ -23,7 +53,15 @@ const SignInForm = () => {
 
 			<section className="space-y-2">
 				<Label htmlFor="email">Email</Label>
-				<Input id="email" type="email" autoComplete="off" />
+				<Input autoComplete="off" id="email" {...register('email')} />
+
+				{!errors.email ? (
+					<p className="h-4"></p>
+				) : (
+					<p className="h-4 text-sm text-red-500">
+						{errors.email?.message as string}
+					</p>
+				)}
 			</section>
 
 			<section className="space-y-2">
@@ -34,7 +72,11 @@ const SignInForm = () => {
 					</Label>
 				</div>
 				<div className="relative">
-					<Input id="password" type={showPassword ? 'text' : 'password'} />
+					<Input
+						type={showPassword ? 'text' : 'password'}
+						id="password"
+						{...register('password')}
+					/>
 					{showPassword ? (
 						<EyeOff
 							className="absolute right-2 top-2 hover:cursor-pointer hover:text-slate-500"
@@ -47,6 +89,14 @@ const SignInForm = () => {
 						/>
 					)}
 				</div>
+
+				{!errors.password ? (
+					<p className="h-4"></p>
+				) : (
+					<p className="h-4 text-sm text-red-500">
+						{errors.password?.message as string}
+					</p>
+				)}
 			</section>
 
 			<section className="space-y-4">
